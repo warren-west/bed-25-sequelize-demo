97d1731f49d7284045fb4f6253abb8157513f0f9
warren-west <teacher.warren.english@gmail.com>
2026-02-04 11:17:59 +0100

    feat: CREATE & DELETE functionality for teams, drivers, and circuits tables.

---
+++ models/index.js
@@ -0,0 +1,4 @@
+/**
+ * Models are classes that represent entities in our database.
+ * Models are also sometimes called "domain objects".
+ */
--- server.js
+++ server.js
@@ -20,14 +20,14 @@
  */
 
 // start off by importing the sequelize dependency:
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes } = require('sequelize')
 
 // create a new instance of the Sequelize class
 // an instance of the Sequelize class represents a connection to the DB
 // similarly to how we created a connection with mysql2
 const sequelize = new Sequelize({
     host: 'localhost',
-    database: 'universitydb',
+    database: 'formulaonedb', // use a fresh database
     username: 'root',
     password: 'admin',
     dialect: 'mysql' // we choose mysql
@@ -54,3 +54,112 @@ sequelize.authenticate()
 //         console.log(error)
 //     }
 // }
+
+/**
+ * We're taking a "code-first" approach to generating a database schema.
+ * Before, we used SQL commands in MySql Workbench to create tables and fields.
+ * Now, we're going to use JavaScript (with Sequelize) to do the same.
+ * Remember, Sequelize is a tool that generates SQL in the background when we use it's helper methods. 
+ */
+
+//#region CREATE Models
+/** CREATE DATABASE MODELS (tables) */
+const teamsTable = sequelize.define('teams', {
+    // IDs (PKs) can either be left for Sequelize to create with defaults
+    // or they can be created explicitly by us
+    teamName: DataTypes.STRING
+}, {
+    timestamps: false // do not create the columns for "createdAt" and "updatedAt"
+})
+
+const driversTable = sequelize.define('drivers', {
+    driverName: DataTypes.STRING,
+    birthDate: DataTypes.DATE
+}, {
+    timestamps: false
+})
+
+const circuitsTable = sequelize.define('circuits', {
+    circuitName: DataTypes.STRING
+}, {
+    timestamps: false
+})
+//#endregion
+
+// #region Populate Database
+async function populateDatabase() {
+    driversTable.create({ driverName: "Lewis Hamilton", birthDate: "1985-12-01", id: 1 })
+    driversTable.create({ driverName: "Charles LeClerk", birthDate: "1992-12-01", id: 2 })
+    driversTable.create({ driverName: "Max Verstappen", birthDate: "1990-12-01", id: 3 })
+    
+    circuitsTable.create({ circuitName: "Silverstone", id: 1 })
+    circuitsTable.create({ circuitName: "Abu Dhabi", id: 2 })
+    circuitsTable.create({ circuitName: "Monza", id: 3 })
+    
+    teamsTable.create({ teamName: "Ferrari", id: 1 })
+    teamsTable.create({ teamName: "Red Bull", id: 2 })
+}
+
+// #endregion
+
+// Synchronize the "sequelize" object with the state of the database:
+sequelize.sync({ alter: true })
+    .then(() => {
+        console.log("database updated...")
+    })
+    .catch(() => {
+        console.log("error updating database...")
+    })
+
+
+const express = require('express')
+const server = express()
+
+// We'll start defining the callback functions in our endpoints as ASYNC
+// because database operations are asynchronous
+// Fetching, inserting, updating, and deleting things from the DB are all ASYNC (they take time)
+server.get('/', async (req, res) => {
+    // fetch all the data from the database
+    // drivers
+    const driverResults = await driversTable.findAll()
+    // teams
+    const teamResults = await teamsTable.findAll()
+    // circuits
+    const circuitResults = await circuitsTable.findAll()
+
+    // log out the results:
+    console.log(driverResults)
+    console.log(teamResults)
+    console.log(circuitResults)
+
+    // return the data as JSON to the client
+    res.status(200).json({
+        drivers: driverResults,
+        teams: teamResults,
+        circuits: circuitResults,
+    })
+    return
+})
+
+// an endpoint for populating the database
+server.post('/populate', async (req, res) => {
+    await populateDatabase()
+    res.status(201).json({ message: "Database Populated!" })
+})
+
+server.delete('/', async (req, res) => {
+    console.log('PING')
+    // we want to delete one record from each table
+    // delete a driver
+    driversTable.destroy({ where: {id:1} })
+    // delete a team
+    teamsTable.destroy({ where: {id:1} })
+    // delete a circuit
+    circuitsTable.destroy({ where: {id:1} })
+
+    res.status(204).json({ message: "Deleted items from the database!" })
+})
+
+server.listen(3000, () => {
+    console.log(`Server is listening on port 3000.`)
+})
